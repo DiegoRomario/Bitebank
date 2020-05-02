73e73ef665ff082138d52d6cc1b555acec69ac3c
Diego Romario <diego.romario@outlook.com>
2020-05-02 06:13:18 -0300

    adding feature item widget

--- srcthree/lib/screens/dashboard.dart
+++ srcthree/lib/screens/dashboard.dart
@@ -21,41 +21,11 @@ class Dashboard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Image.asset('images/bytebank_logo.png'),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Material(
-              color: Theme.of(context).primaryColor,
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(
-                    (MaterialPageRoute(
-                      builder: (context) => ContactList(),
-                    )),
-                  );
-                },
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  width: 150,
-                  height: 100,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Icon(
-                        Icons.people,
-                        color: Theme.of(context).accentColor,
-                        size: 24,
-                      ),
-                      Text(
-                        "Contacts",
-                        style: TextStyle(
-                            color: Theme.of(context).accentColor, fontSize: 16),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+          Row(
+            children: <Widget>[
+              FeatureItem("Transfer", Icons.monetization_on),
+              FeatureItem("Transfers Feed", Icons.description),
+            ],
           )
         ],
       ),
@@ -63,4 +33,49 @@ class Dashboard extends StatelessWidget {
   }
 }
 
-class ContactsList {}
+class FeatureItem extends StatelessWidget {
+  final String name;
+  final IconData icon;
+
+  const FeatureItem(this.name, this.icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context).push(
+              (MaterialPageRoute(
+                builder: (context) => ContactList(),
+              )),
+            );
+          },
+          child: Container(
+            padding: EdgeInsets.all(8),
+            width: 150,
+            height: 100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  icon,
+                  color: Theme.of(context).accentColor,
+                  size: 24,
+                ),
+                Text(
+                  name,
+                  style: TextStyle(
+                      color: Theme.of(context).accentColor, fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
