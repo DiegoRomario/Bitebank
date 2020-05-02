c1faad3c038ec70f6f849b8023e4f6cf0abddc04
Diego Romario <diego.romario@outlook.com>
2020-05-02 05:59:40 -0300

    adding srcthree

---
+++ srcthree/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'screens/dashboard.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.indigo[600],
+        accentColor: Colors.red[300],
+        scaffoldBackgroundColor: Colors.blue[50],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.indigo[800],
+          textTheme: ButtonTextTheme.accent,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Dashboard(),
+    );
+  }
+}
---
+++ srcthree/lib/screens/contact-list.dart
@@ -0,0 +1,92 @@
+import 'package:bytebank/database/dao/contact-dao.dart';
+import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/screens/contact-new.dart';
+import 'package:flutter/material.dart';
+
+class ContactList extends StatefulWidget {
+  @override
+  _ContactListState createState() => _ContactListState();
+}
+
+class _ContactListState extends State<ContactList> {
+  ContactDao _dao = new ContactDao();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contacts"),
+      ),
+      body: FutureBuilder<List<Contact>>(
+        initialData: List(),
+        future: _dao.findAll(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              // return Center(
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Text("Loading..."),
+                    ),
+                  ],
+                ),
+              );
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contact> contacts = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts[index];
+                  return ContactItem(contact);
+                },
+                itemCount: contacts.length,
+              );
+              break;
+          }
+          return Text("Unknown error");
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => ContactNew(),
+            ),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class ContactItem extends StatelessWidget {
+  final Contact _contact;
+
+  const ContactItem(this._contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          _contact.name,
+          style: TextStyle(fontSize: 24),
+        ),
+        subtitle: Text(
+          _contact.accountNumber.toString(),
+          style: TextStyle(fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
